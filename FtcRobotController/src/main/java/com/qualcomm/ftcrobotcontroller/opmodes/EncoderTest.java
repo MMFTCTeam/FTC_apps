f214e2f921891f522f56287ac615a2c57f3d4ffe
SamPhilip <samuel.philip@gmail.com>
2015-11-09 21:01:07 -0500

    V1.0.0.1 Error: throws java.lang.NullPointerException

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EncoderTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EncoderTest.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.Range;
 
 /**
+ * Change
  * Created by sam on 31-Oct-15.
  */
 public class EncoderTest extends OpMode {
@@ -16,10 +17,10 @@ public class EncoderTest extends OpMode {
     public DcMotor OtherMotor;
     public DcMotor BL;
     public DcMotor BR;
-    public ServoController sr;
+    // public ServoController sr;
     public Servo Lbump;
     public Servo Rbump;
-    DcMotorController dc = OtherMotor.getController();
+    // DcMotorController dc = OtherMotor.getController();
 
     @Override
     public void init() {
@@ -30,11 +31,12 @@ public class EncoderTest extends OpMode {
         OtherMotor = hardwareMap.dcMotor.get("m5");
         Rbump = hardwareMap.servo.get("s1");
         Lbump = hardwareMap.servo.get("s2");
+        OtherMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
     }
 
     @Override
     public void loop() {
-       // OtherMotor.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+        OtherMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         long encoderRead = OtherMotor.getCurrentPosition();
         double throttle = gamepad1.right_stick_y;
         Range.clip(throttle, -1, 1);
