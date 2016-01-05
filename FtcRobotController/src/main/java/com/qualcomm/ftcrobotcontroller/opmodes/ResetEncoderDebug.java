2d3f0ffc992b85ac5f15192c4f5f4682789ee5d6
SamPhilip <samuel.philip@gmail.com>
2016-01-05 18:10:06 -0500

    V1.0 Debug program to reset encoders

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ResetEncoderDebug.java
@@ -0,0 +1,61 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+
+/**
+ * Created by sam on 05-Jan-16.
+ */
+public class ResetEncoderDebug extends LinearOpMode {
+    public DcMotor FL;
+    public DcMotor FR;
+    public DcMotor OtherMotor;
+    public DcMotor BL;
+    public DcMotor BR;
+
+    public void initializeRobot() {
+        BL = hardwareMap.dcMotor.get("Bl");
+        FL = hardwareMap.dcMotor.get("Fl");
+        BR = hardwareMap.dcMotor.get("Br");
+        FR = hardwareMap.dcMotor.get("Fr");
+        OtherMotor = hardwareMap.dcMotor.get("a1");
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initializeRobot();
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetry.addData("BL Reset", "Press A to reset");
+            telemetry.addData("BR Reset", "Press B to reset");
+            if (gamepad1.a) {
+                BL.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+                BL.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+                telemetry.addData("BL Reset", "Resetting");
+                waitOneFullHardwareCycle();
+                if (BL.getCurrentPosition() == 0) {
+                    telemetry.addData("BL Reset", "Reset Successful");
+                    sleep(2000);
+                } else {
+                    telemetry.addData("BL Reset", "FAIL!! Either you moved the motor or it didn't reset correctly");
+                    sleep(2000);
+                }
+            }
+            if (gamepad1.b) {
+                BR.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+                BR.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+                telemetry.addData("BR Reset", "Resetting");
+                waitOneFullHardwareCycle();
+                if (BR.getCurrentPosition() == 0) {
+                    telemetry.addData("BR Reset", "Reset Successful");
+                    sleep(2000);
+                } else {
+                    telemetry.addData("BR Reset", "FAIL!! Either you moved the motor or it didn't reset correctly");
+                    sleep(2000);
+                }
+            }
+            waitOneFullHardwareCycle();
+        }
+    }
+}
