7ab797fd8ed9cf48c87f0b8793f6c0c70cb6af31
SamPhilip <samuel.philip@gmail.com>
2016-01-06 17:54:56 -0500

    Autonomous V1.2.3 Alpha
    Works if it resets

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousClimbRed.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousClimbRed.java
@@ -1,6 +1,7 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 
 /**
@@ -8,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
  */
 public class AutonomousClimbRed extends Programbot {
     @Override
-    public void initializeRobot() {
+    public void initializeRobot() throws InterruptedException {
         super.initializeRobot();
     }
 
@@ -16,34 +17,43 @@ public class AutonomousClimbRed extends Programbot {
     public void runOpMode() throws InterruptedException {
         initializeRobot();
         waitForStart();
-        while (BL.getCurrentPosition() < 1024) {
-            BL.setPower(1);
-            BR.setPower(1);
-            FL.setPower(1);
-            FR.setPower(1);
-        }
-        BL.setPower(0);
-        BR.setPower(0);
-        FL.setPower(0);
-        FR.setPower(0);
-        BR.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        BL.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        while(opModeIsActive()) {
+            while (BL.getCurrentPosition() < 1024) {
+                BL.setPower(1);
+                BR.setPower(1);
+                FL.setPower(1);
+                FR.setPower(1);
+            }
+            BL.setPower(0);
+            BR.setPower(0);
+            FL.setPower(0);
+            FR.setPower(0);
+            BR.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+            BL.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+            waitOneFullHardwareCycle();
         /*BL.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        BR.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        BL.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        BR.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);*/
-        while (BR.getCurrentPosition() < 2048) {
-            BL.setPower(-1);
-            FL.setPower(-1);
-            BR.setPower(1);
-            FR.setPower(1);
-        }
-        haltMotors();
-        if(opModeIsActive()) {
-            telemetry.addData("Encoders", BL.getCurrentPosition() + "\n" + BR.getCurrentPosition());
-        }else {
+        BR.setMode(DcMotorController.RunMode.RESET_ENCODERS);*/
+            BL.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+            BR.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+            waitOneFullHardwareCycle();
+            while (BR.getCurrentPosition() < 2048) {
+                BL.setPower(-1);
+                FL.setPower(-1);
+                BR.setPower(1);
+                FR.setPower(1);
+            }
+            BL.setPower(0);
+            BR.setPower(0);
+            FL.setPower(0);
+            FR.setPower(0);
             haltMotors();
+            if (opModeIsActive()) {
+                telemetry.addData("Encoders", BL.getCurrentPosition() + "\n" + BR.getCurrentPosition());
+            } else {
+                haltMotors();
+            }
+            waitOneFullHardwareCycle();
+            break;
         }
-        waitOneFullHardwareCycle();
     }
 }
