30e40bda3a558a4b8ae14de1bf2cec1f514cbd3f
alejan01s <alejan01si@gmail.com>
2016-01-06 17:41:01 -0500

    v1.1

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
@@ -2,31 +2,23 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 
 /**
  * Created by alejandro on 1/4/2016.
  */
 public class TeleOp extends OpMode {
 
-    public boolean armmove = false;
     public DcMotor arm;
-    public double contractThrottle;
+    double contractThrottle;
 
     public void init (){
         arm = hardwareMap.dcMotor.get("a1");
         contractThrottle = gamepad1.right_stick_y;
+        arm.setDirection(DcMotor.Direction.REVERSE);
+        gamepad1.setJoystickDeadzone(0.02f);
     }
     public void loop (){
-        if(gamepad1.b){
-            armmove = true;
-            //while(armmove){
-                arm.setPower(-1);
-        }
-        if (gamepad1.a){
-            armmove=false;
-            //while(contractThrottle<0){
-                arm.setPower(1);
-            //}
-        }
+        arm.setPower(gamepad1.right_stick_y);
     }
 }
