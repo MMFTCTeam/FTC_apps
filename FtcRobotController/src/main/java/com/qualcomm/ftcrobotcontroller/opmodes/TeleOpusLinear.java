1fc5212a495f6cb3a2c03e50ec6051c85d7f8d09
SamPhilip <samuel.philip@gmail.com>
2016-01-28 17:16:12 -0500

    Teleopus 1.9.1
    Armed Update
    +Added Arm control for Player 2

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpusLinear.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpusLinear.java
@@ -6,24 +6,27 @@ import com.qualcomm.robotcore.util.Range;
 
 /**
  * <h1>
- * Created by Team 8521 on 30-Dec-15. <br><br>
- *     Updated on 21-Jan-16
- * TeleOpus Program V1.8.1 </h1>
+ * Created by Sam on 30-Dec-15. <br><br>
+ * Updated on 21-Jan-16
+ * TeleOpus Program V1.9.1 </h1>
  * <h2>
- *     Change log:
+ * Change log:
  * </h2>
  * <p>
- *     V1.8.1 -- <br>
- *     Removed motors and sensors defined by programbot <br>
- *     Migrated code to beta version of Fury Bot <br>
+ * V1.9.1 -- <br>
+ * Added Arm input
+ * V1.8.2 -- <br>
+ * Modified scaleInput<br>
+ * V1.8.1 -- <br>
+ * Removed motors and sensors defined by programbot <br>
+ * Migrated code to beta version of Fury Bot <br>
  * </p>
  * <h2>
- *     Key Mapping: <br>
- *     Analog Sticks: Move Robot <br>
- *     Home button: Reverse Motors <br>
+ * Key Mapping:
  * </h2>
  * <p>
- *
+ * Analog Sticks: Move Robot <br>
+ * Home button: Reverse Motors <br>
  * </p>
  */
 public class TeleOpusLinear extends Fury_Bot {
@@ -32,7 +35,7 @@ public class TeleOpusLinear extends Fury_Bot {
     /**
      * <p>
      * Corresponds to OpMode's init(); <br>
-     * Starting code
+     * Starting code of Program
      * </p>
      */
     @Override
@@ -43,11 +46,12 @@ public class TeleOpusLinear extends Fury_Bot {
     /**
      * Scales input from the joystick <br>
      * Note! Joystick forward returns negative values.
+     *
      * @param value raw gamepad values
      * @return Scaled power values
      */
-    private double scaleInput(double value) {
-        double[] powerval = {0, 0.1, 0.2, 0.2, 0.5, 0.5, 0.5, 0.7, 0.7, 0.9, 1.0};
+    public double scaleInput(double value) {
+        double[] powerval = {0, 0, 0.25, 0.25, 0.5, 0.5, 0.5, 0.75, 0.75, 1.0, 1.0};
         double retVal;
         int index;
         if (value > 0) {
@@ -59,8 +63,10 @@ public class TeleOpusLinear extends Fury_Bot {
         }
         return retVal;
     }
+
     /**
      * Robot Main Loop
+     *
      * @throws InterruptedException
      */
     @Override
@@ -69,6 +75,7 @@ public class TeleOpusLinear extends Fury_Bot {
         waitForStart();
         waitOneFullHardwareCycle();
         while (opModeIsActive()) {
+            String Powers = "BL: \n" + BL.getPower() + "\n BR: \n" + BR.getPower() + "\n FL: \n" + FL.getPower() + "\n FR \n" + FR.getPower();
             BL.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
             FL.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
             BR.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
@@ -117,10 +124,22 @@ public class TeleOpusLinear extends Fury_Bot {
                     // sleep(20);
                 }
             }
+            if (gamepad2.left_bumper) {
+                while (gamepad2.left_bumper) {
+                    Crane.setPower(-1);
+                }
+                Crane.setPower(0);
+            }
+            if (gamepad2.right_bumper) {
+                while (gamepad2.right_bumper) {
+                    Crane.setPower(1);
+                }
+                Crane.setPower(0);
+            }
             telemetry.getTimestamp();
             telemetry.addData("Encoders", BL.getCurrentPosition() + "\n" + BR.getCurrentPosition());
             telemetry.addData("Reversed?", reversed);
-
+            telemetry.addData("Motor's Powers", Powers);
             waitForNextHardwareCycle();
         }
     }
