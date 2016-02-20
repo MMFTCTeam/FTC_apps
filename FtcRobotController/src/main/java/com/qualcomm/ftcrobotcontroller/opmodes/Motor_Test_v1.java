1ded0b0cc6f0cce74bf59211c0ebdce9cfae1748
alejan01s <alejan01si@gmail.com>
2016-02-20 14:09:53 -0500

    v1.1

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Motor_Test_v1.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Motor_Test_v1.java
@@ -4,12 +4,15 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by trevo+aleja on 02/19/2016.
  */
 public class Motor_Test_v1 extends LinearOpMode{
 
+    double cThrottle;
+    double eThrottle;
 
     public DcMotor FL;
     public DcMotor FR;
@@ -34,7 +37,24 @@ public class Motor_Test_v1 extends LinearOpMode{
         hook = hardwareMap.dcMotor.get("Hook");
         hook.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         hook.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        Ext.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        Ext.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        Crane.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        Crane.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
 
+    }
+    public double scaleInput(double value) {
+        double[] powerval = {0, 0.1, 0.2, 0.2, 0.5, 0.5, 0.5, 0.7, 0.7, 0.9, 1.0};
+        double retVal;
+        int index;
+        if (value > 0) {
+            index = (int) Math.abs(Range.clip(value * 10, 0, 10));
+            retVal = powerval[index];
+        } else {
+            index = (int) Math.abs(Range.clip(value * -10, 0, 10));
+            retVal = -powerval[index];
+        }
+        return retVal;
     }
     @Override
     public void runOpMode() throws InterruptedException{
@@ -43,14 +63,36 @@ public class Motor_Test_v1 extends LinearOpMode{
         waitOneFullHardwareCycle();
         while(opModeIsActive()) {
             //drive train
-            FL.setPower(-gamepad1.left_stick_y);
-            BL.setPower(-gamepad1.left_stick_y);
-            FR.setPower(gamepad1.right_stick_y);
-            BR.setPower(gamepad1.right_stick_y);
+            FL.setPower(scaleInput(-gamepad1.left_stick_y));
+            BL.setPower(scaleInput(-gamepad1.left_stick_y));
+            FR.setPower(scaleInput(gamepad1.right_stick_y));
+            BR.setPower(scaleInput(gamepad1.right_stick_y));
             //arm
-            Ext.setPower(-gamepad2.left_stick_y);
-            Crane.setPower(-gamepad2.right_stick_y);
-
+            if (!gamepad2.guide) {
+                cThrottle = -gamepad2.right_stick_y;
+                if (Crane.getCurrentPosition() < 0) {
+                    Crane.setPower(cThrottle > 0 ? cThrottle : 0);
+                } else if (Crane.getCurrentPosition() > 24000) {
+                    Crane.setPower(cThrottle < 0 ? cThrottle : 0);
+                } else {
+                    Crane.setPower(cThrottle);
+                }
+                // end Crane Limits
+                // begin Extension limits
+                eThrottle = -gamepad2.left_stick_y;
+                if (Ext.getCurrentPosition() > 0) {
+                    Ext.setPower(eThrottle > 0 ? eThrottle : 0);
+                } else if (Ext.getCurrentPosition() < -14750) {
+                    Ext.setPower(eThrottle < 0 ? eThrottle : 0);
+                } else {
+                    Ext.setPower(eThrottle);
+                }
+            }
+            // Override limits
+            else {
+                Crane.setPower(-gamepad2.right_stick_y);
+                Ext.setPower(-gamepad2.left_stick_y);
+            }
             //hook right and left bumper
             if(gamepad1.right_bumper){
                // while(hook.getCurrentPosition()<500) {
@@ -70,6 +112,8 @@ public class Motor_Test_v1 extends LinearOpMode{
                 hook.setPower(0);
             }
             telemetry.addData("Hook Encoder", hook.getCurrentPosition());
+            telemetry.addData("Extension Encoder", Ext.getCurrentPosition());
+            telemetry.addData("Crane Encoder", Crane.getCurrentPosition());//-13852
             waitForNextHardwareCycle();
         }
     }
